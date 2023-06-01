1d4271fb630698a7809e827f502843da3578bc93
Evan Norton <evan@retro-mmo.com>
2023-06-01 04:23:23 -0400

    Title sprite

--- src/functions/run.ts
+++ src/functions/run.ts
@@ -1,7 +1,13 @@
-import { init } from "pigeon-mode-game-library";
+import { Sprite, init } from "pigeon-mode-game-library";
 
 const run = (): void => {
   console.log("Hello World");
+
+  new Sprite("title", {
+    x: 0,
+    y: 0,
+  });
+
   init();
 };
 
