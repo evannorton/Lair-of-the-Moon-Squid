91c6085f07a6610d1d4058ea838c1fd2cf8de588
Evan Norton <evan@retro-mmo.com>
2023-06-16 09:14:01 -0400

    Add left click handler for main menu

--- src/functions/run.ts
+++ src/functions/run.ts
@@ -1,4 +1,4 @@
-import { Sprite, init } from "pigeon-mode-game-library";
+import { InputHandler, Sprite, init } from "pigeon-mode-game-library";
 
 const run = (): void => {
   console.log("Hello World");
@@ -6,6 +6,13 @@ const run = (): void => {
   new Sprite("title", {
     x: 0,
     y: 0,
+  });
+
+  new InputHandler({
+    leftClick: true,
+    onInput: (): void => {
+      console.log("Advance main menu");
+    },
   });
 
   init();
