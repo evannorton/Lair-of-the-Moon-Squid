33041846c5dde5bcca799464caee79b69c5100a6
Evan Norton <evan@retro-mmo.com>
2023-10-18 03:24:53 -0400

    add achievement ids

--- src/game/global/achievements.ts
+++ src/game/global/achievements.ts
@@ -2,12 +2,14 @@ import { createAchievement } from "pixel-pigeon";
 
 export const testAchievement1ID: string = createAchievement({
   description: "Click through the title screen 1.",
+  id: "test-1",
   imagePath: "achievements/test",
   name: "Test Achievement 1",
 });
 
 export const testAchievement2ID: string = createAchievement({
   description: "Click through the title screen 2.",
+  id: "test-2",
   imagePath: "achievements/test",
   name: "Test Achievement 2",
 });
