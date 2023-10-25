5c0331ae48be3c03c71384a31676116ffcfbb401
Evan Norton <evan@retro-mmo.com>
2023-10-24 22:42:13 -0400

    Add locked achievement

--- src/game/global/achievements.ts
+++ src/game/global/achievements.ts
@@ -14,9 +14,16 @@ export const testAchievement2ID: string = createAchievement({
   name: "Test Achievement 2",
 });
 
+export const testLockedAchievementID: string = createAchievement({
+  description: "You cannot unlock this xD",
+  id: "locked",
+  imagePath: "achievements/test",
+  name: "Test Locked Achievement",
+});
+
 export const testAchievement3ID: string = createAchievement({
-  description: "Click through the title screen 3.",
+  description: "Click through the title screen 3 asdf asdf asdfasdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdfasdfasdf asdf asdf asdfasdf asdf asdf asdf asdf asdf.",
   id: "test-3",
   imagePath: "achievements/test",
-  name: "Test Achievement 3",
+  name: "Test Achievement 3 asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdf asdfasdf asdf asdfasdf asdf asdf asdfasdfasdfvasdf    asdf asdf asdf",
 });
