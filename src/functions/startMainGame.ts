c2312af434da5fe116140d12cb110e30fe3c9ce4
Evan Norton <evan@retro-mmo.com>
2024-03-11 00:47:24 -0400

    run linter

--- src/functions/startMainGame.ts
+++ src/functions/startMainGame.ts
@@ -1,8 +1,8 @@
 import { createPlayer } from "./createPlayer";
 import { goToLevel, playAudioSource, stopAudioSource } from "pixel-pigeon";
 import { musicVolumeChannelID } from "../volumeChannels";
-import { state } from "../state";
 import { startPhase1 } from "./startPhase/startPhase1";
+import { state } from "../state";
 
 export const startMainGame = (): void => {
   state.setValues({ isAtTitle: false });
